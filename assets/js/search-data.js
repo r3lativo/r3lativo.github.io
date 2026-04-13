743e1092db602deab4265b4b176e0cea2e294954
r3lativo <39198696+r3lativo@users.noreply.github.com>
2026-04-13 22:01:54 +0000

    Deploying to gh-pages from @ r3lativo/r3lativo.github.io@6afecc80acc46701c231fcc222fa62c025e5c213 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,8 +37,20 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "news-we-are-presenting-our-work-at-clic-it-in-cagliari",
-          title: 'We are presenting our work at CLiC-iT in Cagliari!',
+        },{id: "news-started-my-research-internship-at-articulab-within-the-almanach-team-at-inria-paris",
+          title: 'Started my research internship at ArticuLab within the ALMAnaCH team at Inria Paris....',
+          description: "",
+          section: "News",},{id: "news-continued-at-articulab-to-write-my-master-s-thesis-on-situated-dialogue-and-common-ground",
+          title: 'Continued at ArticuLab to write my Master’s thesis on situated dialogue and common...',
+          description: "",
+          section: "News",},{id: "news-marika-is-presenting-our-work-mllms-construction-company-at-clic-it-2025-in-cagliari",
+          title: 'Marika is presenting our work MLLMs Construction Company at CLiC-iT 2025 in Cagliari!...',
+          description: "",
+          section: "News",},{id: "news-graduated-with-a-master-s-degree-in-cognitive-science-from-cimec-university-of-trento",
+          title: 'Graduated with a Master’s degree in Cognitive Science from CIMeC, University of Trento!...',
+          description: "",
+          section: "News",},{id: "news-joined-almanach-at-inria-paris-as-a-research-engineer-working-with-djamé-seddah-on-computational-propaganda",
+          title: 'Joined ALMAnaCH at Inria Paris as a Research Engineer, working with Djamé Seddah...',
           description: "",
           section: "News",},{
       id: 'light-theme',
